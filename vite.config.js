a4447bf5422466fa8f6fd27ecd66ba610669d775
Nur Mohammad Ansari <ansarinurmohammad19@gmail.com>
2025-10-29 19:04:28 +0530

    fix: correct defineConfig import and config file location

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineconfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
